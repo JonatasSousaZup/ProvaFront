01f264dc5e0f192c034caec6c7f7d5fb75457d2b
Tiago Souza <tiago.souza@zup.com.br>
2020-02-17 14:44:23 -0300

    Criar component Menu

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -8,7 +8,7 @@ export default function Header() {
         <div>
             <Container>
                     <img src="https://uploads-ssl.webflow.com/5cac6236f8d44ddee118d97c/5cb08cdd17edca8cbfc171a6_zup-neon.png" />
-                    <input placeholder="Buscar" />
+                    <input placeholder="   Buscar" />
                     <FaUserCircle className="userIcon" />
             </Container>
         </div>
---
+++ src/Components/Menu/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+   height: 80px;
+    position: absolute;
+    left: 5%;
+    right: 5%;
+    padding: 0 30px;
+    background: rgb(0, 55, 82);
+    color: #FFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
