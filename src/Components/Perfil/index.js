cf65ee562cd4822615687ee218697bb1efb11628
Jonatas Oliveira <Jonatas.sousa@zup.com.br>
2020-04-02 12:01:21 -0300

    estilizando o perfil

--- src/Components/Perfil/index.js
+++ src/Components/Perfil/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Container, Header, Section } from './styles';
+import { Container, Header, Section, Button, Img } from './styles';
 import { FaChevronCircleLeft } from 'react-icons/fa';
 
 export default function Perfil() {
     return (
         <Container>
             <Header>
-                <FaChevronCircleLeft className="leftIcon" />
+                <Button>
+                    <FaChevronCircleLeft className="leftIcon" />
+                </Button>
             </Header>
 
             <Section>
-                {/* <img src="https://miro.medium.com/max/570/1*EelUYA6BOTNXtuRjSlaqHw.png"/> */}
+                <Img src="https://miro.medium.com/max/570/1*EelUYA6BOTNXtuRjSlaqHw.png" />
             </Section>
         </Container>
     );
