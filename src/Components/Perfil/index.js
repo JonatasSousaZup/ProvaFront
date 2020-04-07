2f03004219f7c2a30f41f79a2014f3988ab9007d
Jonatas Oliveira <Jonatas.sousa@zup.com.br>
2020-04-07 17:44:00 -0300

    estilizando perfil

--- src/Components/Perfil/index.js
+++ src/Components/Perfil/index.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import { Container, Header, Section, Img, Linha } from './styles';
-
 import { FaChevronCircleLeft, FaRegUser } from 'react-icons/fa';
 import { FiMapPin, FiPhone } from 'react-icons/fi';
 import { GoMail, GoCalendar } from 'react-icons/go';
 import { TiKeyOutline } from 'react-icons/ti';
 
-
-
 export default function Perfil() {
     return (
         <Container>
             <Header>
-                 <Linha to="/"> <FaChevronCircleLeft className="leftIcon" /></Linha>
+                <Linha to="/"> <FaChevronCircleLeft className="leftIcon" /></Linha>
             </Header>
 
             <Section>
                 <Img src="https://miro.medium.com/max/570/1*EelUYA6BOTNXtuRjSlaqHw.png" />
-                <span className="Icones" >< FaRegUser /></span>
-                <span className="Icones">< GoMail /></span>
-                <span className="Icones">< GoCalendar /></span>
-                <span className="Icones">< FiMapPin /></span>
-                <span className="Icones">< FiPhone /></span>
-                <span className="Icones">< TiKeyOutline/></span>
-            
+                <FaRegUser className="Icons" />
+                <GoMail className="Icons" />
+                <GoCalendar className="Icons" />
+                <FiMapPin className="Icons" />
+                <FiPhone className="Icons" />
+                <TiKeyOutline className="Icons" />
             </Section>
         </Container>
     );
--- src/Components/Perfil/styles.js
+++ src/Components/Perfil/styles.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-
 export const Container = styled.div`
     height: 100vh;
     margin: 0 auto;
@@ -17,21 +16,6 @@ export const Header = styled.header`
     margin-bottom: auto;
 `;
 
-export const Button = styled.button`
-        height: 40px;
-        width: 40px;
-        margin-left: 18px;
-        margin-top: 18px;
-        background: none;
-        border-style: none;
-
-    .leftIcon {
-        color: #FFF;
-        height: 35px;
-        width: 35px;
-    }
-`;
-
 export const Section = styled.section`
     width: 100%;
     max-width: 600px;
@@ -40,8 +24,13 @@ export const Section = styled.section`
     height: 600px;
     position: absolute;
     background: #FFF;
-    border-radius: 2px;
+    border-radius: 10px;
     box-shadow: 5px 5px 5px #939598;
+
+    .Icons {
+        height: 50px;
+        width: 50px;
+    }
 `;
 
 export const Img = styled.img`
@@ -51,15 +40,13 @@ export const Img = styled.img`
     border-radius: 200px;
     height: 35%;
 `;
-export const Icones = styled.span`
-    
-    position: relative;
-    margin-top: 100px;
-    size: 500px;
-`;
+
 export const Linha = styled(Link)`
-    text-decoration: none;
-    padding: 100px;
-    color: ${({active}) => active ? "#8dc63f" : "inherit"}
-    
+    .leftIcon {
+        color: #FFF;
+        height: 40px;
+        width: 40px;
+        margin-top: 18px;
+        margin-left: 18px;
+    }
 `;
